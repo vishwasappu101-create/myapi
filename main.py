143837b790e6594eb3544984f2890caadeb443fe
vishwasappu101-create <vishwasappu101@gmail.com>
2025-10-30 05:41:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ from fastapi.responses import RedirectResponse
 
 app = FastAPI(title="Vishwa's Calculator API")
 
+# Serve the calculator from the /calc route
 app.mount("/calc", StaticFiles(directory="static", html=True), name="calc")
 
+# Redirect root (/) to /calc automatically
 @app.get("/")
 def root():
     return RedirectResponse(url="/calc")
